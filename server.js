f27309850ec8580863e5ded2a4f845ea1bd13a55
worthyone02 <worthyone02@gmail.com>
2026-02-23 19:50:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_SERVICE_KEY
 );
 
 /* =============================
